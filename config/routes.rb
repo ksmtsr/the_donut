41ef091e22017426ae9f8307a19ab82d20cdf1c5
kamtsr <116964080+ksmtsr@users.noreply.github.com>
2023-05-15 13:24:53 +0000

    トップページレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,26 @@
 Rails.application.routes.draw do
 
-  devise_for :admins, controllers: {
+#↓最初に持ってくる
+  devise_for :admin, controllers: {
     sessions: "admin/sessions"
   }
 
-  # namespace :admin do
-  # end
+  #名前の重複があるときに、衝突を防ぐためにname spaceを使用
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+  end
+
+
 
   devise_for :customers, controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
+  #URLからpublicを消したURLにしたい場合は、scope moduleを使う
+  scope module: :public do
+  root 'homes#top'
+  get 'about' => 'homes#about'
+end
 
 end
